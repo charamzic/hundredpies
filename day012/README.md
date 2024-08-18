5670fc8b1fce06b6cba4f6810321dd31c4b5eeca
honza <charamza@gmail.com>
2024-08-18 12:39:00 +0200

    day twelve, guess the number game

---
+++ day012/README.md
@@ -0,0 +1,4 @@
+# A simple Guess The Number game
+
+There are two difficulty levels - EASY and HARD. Based on that, the player has 10 or 5 guesses respectively. 
+The program gives hints by printing "Too high", or "Too low". A player wins when he guesses the right number.
---
+++ day012/main.py
@@ -0,0 +1,52 @@
+import os
+import random
+
+EASY = 10
+HARD = 5
+
+number_to_guess = random.randint(1, 50)
+
+
+def set_rounds(chosen_level):
+    if chosen_level == 'e':
+        return EASY
+    elif chosen_level == 'h':
+        return HARD
+
+
+def play():
+    level = input(
+        "Type 'e' for easy difficulty or 'h' for hard difficulty: ")
+    rounds = 0
+
+    if level == 'e':
+        rounds = EASY
+        print(f"Chosen difficulty: EASY. You have {EASY} guesses.")
+    elif level == 'h':
+        rounds = HARD
+        print(f"Chosen difficulty: HARD. You have {HARD} guesses.")
+    else:
+        print(f"Wrong input: {level}")
+        play()
+
+    while rounds > 0:
+        guessed = int(input("Guess the number between 1 and 50: "))
+
+        if guessed > number_to_guess:
+            print("Too high.")
+        elif guessed < number_to_guess:
+            print("Too low")
+        else:
+            print(f"\n=== You got it! The right number is {
+                  number_to_guess}. ===")
+            rounds = 0
+
+        rounds -= 1
+        if rounds == 0:
+            print(f"You are out of guesses. The right number was {
+                  number_to_guess}.")
+
+
+while input("\nWanna play Guess The Number game? [y/n]: ") == 'y':
+    os.system("clear")
+    play()
