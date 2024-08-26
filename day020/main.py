7350a44b9c02c213ed4fbc0021fb953d4eb552c0
honza <charamza@gmail.com>
2024-08-26 13:22:57 +0200

    day twenty, snake could not be missing, first part

---
+++ day020/main.py
@@ -0,0 +1,30 @@
+from turtle import Screen, Turtle
+from cobra import Cobra
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Cobra")
+screen.tracer(0)
+
+
+segments = []
+
+cobra = Cobra()
+
+screen.listen()
+screen.onkey(cobra.up, "Up")
+screen.onkey(cobra.down, "Down")
+screen.onkey(cobra.left, "Left")
+screen.onkey(cobra.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    cobra.move()
+
+
+screen.exitonclick()
